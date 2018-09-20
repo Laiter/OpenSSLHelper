ba2d26b744d936e2427949635cbdf84b463a142e
Laiter <laitergg@ya.ru>
2018-09-20 21:11:20 +0300

    add cleaning func, extra logs, creating of rsa signature

--- Keys/gen_project.bat
+++ Keys/gen_project.bat
@@ -25,10 +25,11 @@ if exist ..\Files\!project_name! (
 	(>&2 echo "Directory Files\!project_name! already exists")
 	EXIT /B 1
 )
+:: ============================
 :: create dirs
 if not exist !project_name! mkdir !project_name!
 if not exist ..\Files\!project_name! mkdir ..\Files\!project_name!
-:: create keys
+:: create AES keys
 set /A gen_key_num=1
 set /A master_key_num=1
 (>&2 echo "Generating AES-256")
@@ -38,25 +39,47 @@ for %%f in (%gen_aes_keys_name%) do (
 	call ..\Utils\AES\Gen_AES_256.bat !project_name!\%%f
 	if not !errorlevel!==0 (
 		(>&2 echo "stop, Gen_AES_256.bat error")
+		call :Clean
 		EXIT /B !errorlevel!
 	)
 	set /A master_key_num=1 
 	for %%k in (%master_aes_keys_name%) do (
 		if !gen_key_num! EQU !master_key_num! (>&2 echo "(%%f)%%k = CRYPTOGRAM_!gen_key_num!.BIN")
 		if !gen_key_num! EQU !master_key_num! echo 1|call ..\Utils\AES\call_enc.bat %master_keys_dir%\%%k.hex 00000000000000000000000000000000 !project_name!\%%f.bin > !project_name!\CRYPTOGRAM_!gen_key_num!.BIN
-		set /A master_key_num=master_key_num+1
+		if not !errorlevel!==0 (
+			(>&2 echo "stop, call_enc.bat error !errorlevel!")
+			call :Clean
+			EXIT /B !errorlevel!
 		)
+		set /A master_key_num=master_key_num+1
+	)
 	set /A gen_key_num=gen_key_num+1
 )
+:: create RSA keys
+set /A gen_key_num=1
+set /A master_key_num=1
 (>&2 echo "Generating RSA")
 for %%f in (%gen_rsa_keys_name%) do (
 	(>&2 echo "Work with %%f")  
 	(>&2 echo "..\Utils\RSA\Gen_RSA.bat !project_name!\%%f %rsa_key_length%")
 	call ..\Utils\RSA\Gen_RSA.bat !project_name!\%%f %rsa_key_length%
 	if not !errorlevel!==0 (
-		(>&2 echo "stop, Gen_RSA.bat error")
+		(>&2 echo "stop, Gen_RSA.bat error !errorlevel!")
+		call :Clean
 		EXIT /B !errorlevel!
 	)
+	set /A master_key_num=1 
+	for %%k in (%master_rsa_keys_name%) do (
+		if !gen_key_num! EQU !master_key_num! (>&2 echo "..\Utils\RSA\call_dgst.bat %master_keys_dir%\%%k_PRIV.PEM !project_name!\%%f_PUB.PEM")
+		if !gen_key_num! EQU !master_key_num! echo 1|call ..\Utils\RSA\call_dgst.bat %master_keys_dir%\%%k_PRIV.PEM !project_name!\%%f_PUB.PEM
+		if not !errorlevel!==0 (
+			(>&2 echo "stop, call_dgst.bat error = !errorlevel!")
+			call :Clean
+			EXIT /B !errorlevel!
+		)
+		set /A master_key_num=master_key_num+1
+	)
+	set /A gen_key_num=gen_key_num+1
 )
 :: copy Encrypt\Decrypt bat files
 (>&2 echo "xcopy /s ..\Utils\template ..\Files\!project_name!")
@@ -70,9 +93,19 @@ if not !errorlevel!==0 (
 call gen_license.bat
 if not !errorlevel!==0 (
 	(>&2 echo "get_license.bat error !errorlevel!")
+	call :Clean
+	EXIT /B !errorlevel!
+)
+:: zip deploy
+call :ZipDeploy
+if not !errorlevel!==0 (
+	(>&2 echo "ZipDeploy error !errorlevel!")
+	call :Clean
 	EXIT /B !errorlevel!
 )
 :: ========== FUNCTIONS ==========
+EXIT /B
+
 :ZipDeploy
 cd %project_name%
 if not "%deploy_zip_password%"=="" (
@@ -92,4 +125,17 @@ for %%d in (..\..\Utils\deploy\*.txt) do (
 	)
 )
 cd ..
+EXIT /B
+
+:Clean
+(>&2 echo "Cleaning...")
+cd /d %~dp0
+if exist !project_name! (
+	(>&2 echo "delete dir !project_name!")
+	rmdir /s !project_name!
+) 
+if exist ..\Files\!project_name! (
+	(>&2 echo "delete dir ..\Files\!project_name!")
+	rmdir /s ..\Files\!project_name!
+) 
 EXIT /B
--- Utils/RSA/call_dgst.bat
+++ Utils/RSA/call_dgst.bat
@@ -12,11 +12,13 @@ setlocal EnableDelayedExpansion
 (>&2 echo "Operations: 1 - Sign SHA256, 2 - Verify SHA256")
 set /p CryptoOperation=
 IF %CryptoOperation%==1 (
+	(>&2 echo "call %~dp0\SingVerify_RSA.bat %2 -sign %1 -out %2.SIGN")
     call %~dp0\SingVerify_RSA.bat %2 -sign %1 -out %2.sign
 	(>&2 echo "------------------------- call_dgst")
 	EXIT /B !errorlevel!
 )
 IF %CryptoOperation%==2 (
+	(>&2 echo "call %~dp0\SingVerify_RSA.bat %2 -verify %1 -signature %2.sign")
     call %~dp0\SingVerify_RSA.bat %2 -verify %1 -signature %2.sign
 	(>&2 echo "------------------------- call_dgst")
 	EXIT /B !errorlevel!
